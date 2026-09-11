44ee910e8121237c43030695edbe76f4fbe7d34d
jordan23bull-afk <jordan23bull-afk@users.noreply.github.com>
2026-09-11 16:37:23 +0300

    chore: remove dev self-check scripts from repo

--- _check_tf_consistency.py
+++
@@ -1,8 +0,0 @@
-import re
-
-be = open("backend/core/times.py", encoding="utf-8").read()
-fe = open("frontend/js/constants.js", encoding="utf-8").read()
-b = set(re.findall(r'"(\w+)": (\d+)', be))
-f = set(re.findall(r'"(\w+)": (\d+)', fe))
-assert b == f, b ^ f
-print("TF_SECONDS backend==frontend OK")
